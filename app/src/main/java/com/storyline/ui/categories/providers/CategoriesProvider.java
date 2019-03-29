def9f77dd63866f6d9a54fc6ce899e18a0547054
liranahum <liran.nahum@gmail.com>
2019-03-29 12:20:07 +0300

    Add new categories

--- app/src/main/java/com/storyline/ui/categories/providers/CategoriesProvider.java
+++ app/src/main/java/com/storyline/ui/categories/providers/CategoriesProvider.java
@@ -1,5 +1,6 @@
 package com.storyline.ui.categories.providers;
 
+import com.storyline.R;
 import com.storyline.ui.categories.model.Category;
 
 import java.util.ArrayList;
@@ -12,15 +13,38 @@ public class CategoriesProvider {
 
         ArrayList<Category> categories = new ArrayList<>();
 
-        List<String> categoryLines = new ArrayList<>();
-        categoryLines.add("Once upon a time...");
-        categoryLines.add("Once upon a time...");
-        categoryLines.add("Once upon a time...");
+        List<String> adventureCategoryLines = new ArrayList<>();
+        adventureCategoryLines.add("The story begins when we met a shy dino...");
+        adventureCategoryLines.add("I found this treasure map and i had to fly to...");
+        adventureCategoryLines.add("Quietly we entered into the tomb to find...");
 
-        categories.add(new Category("Adventures", 0, categoryLines));
-        categories.add(new Category("Action", 0, categoryLines));
-        categories.add(new Category("Horror", 0, categoryLines));
-        categories.add(new Category("Love", 0, categoryLines));
+        categories.add(new Category("Adventures",
+                R.drawable.storyline_theme_bg_adventure,
+                R.drawable.storyline_story_logo_adventure,
+                0,
+                adventureCategoryLines));
+
+        List<String> fairytaleCategoryLines = new ArrayList<>();
+        fairytaleCategoryLines.add("Once upon a time the kingdom was under...");
+        fairytaleCategoryLines.add("In a faraway land there was a magical...");
+        fairytaleCategoryLines.add("The princess was sad the day because...");
+
+        categories.add(new Category("Fairytale",
+                R.drawable.storyline_theme_bg_fairytale,
+                R.drawable.storyline_story_logo_fairytale,
+                0,
+                fairytaleCategoryLines));
+
+        List<String> superHeroCategoryLines = new ArrayList<>();
+        superHeroCategoryLines.add("The night was dark and the city was in trouble...");
+        superHeroCategoryLines.add("The giant eye ball is attack us again...");
+        superHeroCategoryLines.add("We need someone to save us from the...");
+
+        categories.add(new Category("Super Hero",
+                R.drawable.storyline_theme_bg_superhero,
+                R.drawable.storyline_story_logo_superhero,
+                0,
+                superHeroCategoryLines));
 
         return categories;
     }
