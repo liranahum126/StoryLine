24a96a6e0aaa31ef889dc3d3e07a49077f8fbaa1
DESKTOP-LIF8HRQ\zeevi <zeevis@gmail.com>
2019-11-14 15:36:14 +0200

    on process

--- app/src/main/java/com/storyline/ui/chat/ChatFragment.java
+++ app/src/main/java/com/storyline/ui/chat/ChatFragment.java
@@ -25,7 +25,6 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.storyline.FriendlyMessage;
 import com.storyline.InterActiveFriend;
